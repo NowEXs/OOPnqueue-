57e84a272c4a923ef5d0486aeda4bfc0b11beb09
NowEXs <66070285@kmitl.ac.th>
2024-03-12 23:57:33 +0700

    fix for run
    -fix to run
    -create testPanel for ฉันได้ลองดูว่ารันได้หรือไม่
    -เปลี่ยน languageLv 21 -> 17 เพื่อให้สามารถรัน swing ได้

--- OOP/src/TA.java
+++ OOP/src/TA.java
@@ -1,12 +1,17 @@
-public class TA implements Checkable extends User {
+public class TA extends User implements Checkable{
     private String ta_password;
-    public boolean isCheckable(){
-
-    }
-    public boolean isCheckable(){
-
-    }
+//    public boolean isCheckable(){
+//
+//    }
+//    public boolean isCheckable(){
+//
+//    } for run
     public TA(){
+
+    }
+
+    @Override
+    public void Checkable() {
 
     }
 }
