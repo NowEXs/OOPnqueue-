e3cdab231df02687fc2766d95ce033f017c122ef
peerapat138 <66070138@kmitl.ac.th>
2024-03-12 18:48:42 +0700

    Create OnClick.java

---
+++ OOP/src/OnClick.java
@@ -0,0 +1,10 @@
+import java.awt.event.ActionListener;
+
+public abstract class OnClick implements ActionListener {
+    public ActionListener pressConfirm(){
+
+    }
+    public ActionListener pressCancel(){
+        
+    }
+}
