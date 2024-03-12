4da872264ddd5f95fb5e00781bf656b84f6a8966
NowEXs <66070285@kmitl.ac.th>
2024-03-13 00:13:45 +0700

    add libraries for excel

--- OOP/src/DeskPanel.java
+++ OOP/src/DeskPanel.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
 public class DeskPanel implements RoleChecker{
     private Computer desk; // computer desk ต้อง cast เป็น Jbutton ในภายหลัง ถ้ากดแล้ว is professor false จะเปิดpopup ของ TA or Student
     private JLabel deskLabel; // ชื่อโต๊ะ
