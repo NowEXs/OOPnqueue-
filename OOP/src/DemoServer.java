8af3d4d7de8d3017d751ae195e5711cc0cb6007e
NowEXs <66070285@kmitl.ac.th>
2024-03-15 14:36:41 +0700

    Fix Connection
    ไม่รู้ว่ามันจะส่งไปอีกเครื่องได้จริงมั้ย

--- OOP/src/DemoServer.java
+++ OOP/src/DemoServer.java
@@ -7,14 +7,19 @@ public class DemoServer {
             public void run() {
                 try( ServerSocket welcomeSocket = new ServerSocket(1111);
                     PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream("Comment.txt", true),"UTF-8")))) {
-                    System.out.println("Please Wait...");
-                    Socket soc = welcomeSocket.accept();
-                    System.out.println("Connection Complete");
-                    BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-                    String txt = in.readLine();
-                    pw.println(txt);
-                    pw.flush();
-                    soc.close();
+                    while(true){
+                        System.out.println("Please Wait...");
+                        Socket soc = welcomeSocket.accept();
+                        System.out.println("Connection Complete");
+                        BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+                        String txt = in.readLine();
+                        pw.println(txt);
+                        pw.flush();
+                        soc.close();
+                    }
+                }
+                catch (ConnectException e){
+                    System.out.println("wait...");
                 }
                 catch (BindException e){
                     throw new RuntimeException(e);
