e800a88c87d42ef8f0d53a5e8ee8509606fc2fe3
jazzpu <144272963+jazzpu@users.noreply.github.com>
2024-03-22 23:31:33 +0700

    undo merge

--- OOP/src/Student.java
+++ OOP/src/Student.java
@@ -1,7 +1,30 @@
 public class Student extends User{
-    private String lab_name, std_name, std_id;
+    private String  std_name, std_id, lab_name;
 
     public Student(){
-
+        this("", "","");
+    }
+    public Student(String std_name, String std_id, String lab_name){
+        this.std_name = std_name;
+        this.std_id = std_id;
+        this.lab_name = lab_name;
+    }
+    public void setStd_name(String std_name){
+        this.std_name = std_name;
+    }
+    public String getStd_name(){
+        return this.std_name;
+    }
+    public void setLab_name(String lab_name) {
+        this.lab_name = lab_name;
+    }
+    public String getLab_name(){
+        return this.lab_name;
+    }
+    public void setStd_id(String std_id){
+        this.std_id = std_id;
+    }
+    public String getStd_id(){
+        return std_id;
     }
 }
