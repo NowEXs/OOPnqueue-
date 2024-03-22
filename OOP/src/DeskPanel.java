beb1b839d8f7fa299bb8c8993b04646f691303b3
NettaeKrub <Undaaa12345@gmail.com>
2024-03-22 22:54:50 +0700

    DeskPanel Created

--- OOP/src/DeskPanel.java
+++ OOP/src/DeskPanel.java
@@ -1,14 +1,31 @@
 import javax.swing.*;
-public class DeskPanel implements RoleChecker{
-    private Computer desk; // computer desk ต้อง cast เป็น Jbutton ในภายหลัง ถ้ากดแล้ว is professor false จะเปิดpopup ของ TA or Student
-    private JLabel deskLabel; // ชื่อโต๊ะ
 
+public class DeskPanel extends JPanel{
+    private JPanel area_com;
+    private JLabel wood;
 
-    public DeskPanel(){
+    public DeskPanel() {
+        area_com = new JPanel();
+        wood = new JLabel();
 
-    }
-    @Override
-    public void userType() {
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        area_com.setOpaque(false);
+
+        GroupLayout area_comLayout = new GroupLayout(area_com);
+        area_com.setLayout(area_comLayout);
+        area_comLayout.setHorizontalGroup(
+                area_comLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGap(0, 770, Short.MAX_VALUE)
+        );
+        area_comLayout.setVerticalGroup(
+                area_comLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGap(0, 550, Short.MAX_VALUE)
+        );
+
+        add(area_com, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 20, 770, 550));
+
+        wood.setIcon(new ImageIcon(getClass().getResource("/Image/left.png"))); // NOI18N
+        add(wood, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
     }
 }
