fe527984b26cd79b3419e628f05a25829391b1a8
jazzpu <144272963+jazzpu@users.noreply.github.com>
2024-03-22 23:54:19 +0700

    reservation & student mod

--- OOP/src/Student.java
+++ OOP/src/Student.java
@@ -1,7 +1,36 @@
 public class Student extends User{
-    private String lab_name, std_name, std_id;
+    private String std_name, lab_name, std_id;
 
     public Student(){
+        this("", "","");
+    }
+    public Student(String std_name,String  lab_name,String  std_id){
+        this.std_id = std_id;
+        this.std_name = std_name;
+        this.lab_name = lab_name;
+    }
 
+    public String getLab_name() {
+        return lab_name;
+    }
+
+    public void setLab_name(String lab_name) {
+        this.lab_name = lab_name;
+    }
+
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    public String getName() {
+        return super.getName();
+    }
+
+    public String getStd_id() {
+        return std_id;
+    }
+
+    public void setStd_id(String std_id) {
+        this.std_id = std_id;
     }
 }
