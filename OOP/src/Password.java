f3b51f6174429a7e31206be5d03533b6d9d539fe
NettaeKrub <Undaaa12345@gmail.com>
2024-03-23 14:58:59 +0700

    ก๊อปไฟล์มาจาก main

--- OOP/src/Password.java
+++ OOP/src/Password.java
@@ -1,11 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-
 
 import java.awt.Color;
-
+import java.awt.Font;
+import java.io.File;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -23,6 +21,25 @@ public class Password extends javax.swing.JFrame {
     public Password() {
         initComponents();
         password_pwf.setFocusable(true);
+
+        try{
+            File fontStyle_apple = new File("OOP/src/Font/Big Apple 3PM.ttf");
+            File fontStyle_minecraft = new File("OOP/src/Font/minecraft_font.ttf");
+            Font font_welcome = Font.createFont(Font.TRUETYPE_FONT,fontStyle_apple).deriveFont(35f);
+            Font font_nqueue = Font.createFont(Font.TRUETYPE_FONT,fontStyle_apple).deriveFont(60f);
+            Font font_password = Font.createFont(Font.TRUETYPE_FONT,fontStyle_apple).deriveFont(30f);
+            Font font_enter = Font.createFont(Font.TRUETYPE_FONT,fontStyle_minecraft).deriveFont(22f);
+            Font font_confirm = Font.createFont(Font.TRUETYPE_FONT,fontStyle_apple).deriveFont(18f);
+            welcome_l.setFont(font_welcome);
+            nqueue_l.setFont(font_nqueue);
+            password_l.setFont(font_password);
+            enter_l.setFont(font_enter);
+        }catch(Exception e){
+            e.printStackTrace();
+
+        }
+
+
     }
 
     /**
@@ -41,7 +58,6 @@ public class Password extends javax.swing.JFrame {
         password_p = new javax.swing.JPanel();
         password_l = new javax.swing.JLabel();
         password_pwf = new javax.swing.JPasswordField();
-        confirm_l = new javax.swing.JLabel();
         confirm_bt = new javax.swing.JButton();
         bg = new javax.swing.JLabel();
 
@@ -54,18 +70,18 @@ public class Password extends javax.swing.JFrame {
         nqueue_l.setText("N - QUEUE ");
         nqueue_l.setToolTipText("");
         nqueue_l.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
-        getContentPane().add(nqueue_l, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 100, 390, 70));
+        getContentPane().add(nqueue_l, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 120, 390, 70));
 
         welcome_l.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
         welcome_l.setForeground(new java.awt.Color(255, 255, 255));
         welcome_l.setText("Welcome to");
-        getContentPane().add(welcome_l, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 50, 360, -1));
+        getContentPane().add(welcome_l, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 70, 360, -1));
         getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 68, 37, -1));
 
         enter_l.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         enter_l.setForeground(new java.awt.Color(255, 255, 255));
         enter_l.setText("Enter the secret code !");
-        getContentPane().add(enter_l, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 280, -1, -1));
+        getContentPane().add(enter_l, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 290, -1, -1));
 
         password_p.setBackground(new java.awt.Color(255, 255, 255));
         password_p.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
@@ -73,7 +89,7 @@ public class Password extends javax.swing.JFrame {
 
         password_l.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         password_l.setText("Password");
-        password_p.add(password_l, new org.netbeans.lib.awtextra.AbsoluteConstraints(34, 9, 118, 52));
+        password_p.add(password_l, new org.netbeans.lib.awtextra.AbsoluteConstraints(34, 9, 190, 52));
 
         password_pwf.setBackground(new java.awt.Color(235, 235, 235));
         password_pwf.setForeground(new java.awt.Color(153, 153, 153));
@@ -92,26 +108,34 @@ public class Password extends javax.swing.JFrame {
                 password_pwfActionPerformed(evt);
             }
         });
-        password_p.add(password_pwf, new org.netbeans.lib.awtextra.AbsoluteConstraints(178, 9, 633, 52));
+        password_p.add(password_pwf, new org.netbeans.lib.awtextra.AbsoluteConstraints(191, 9, 620, 52));
 
         getContentPane().add(password_p, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 330, 820, 70));
 
-        confirm_l.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        confirm_l.setForeground(new java.awt.Color(255, 255, 255));
-        confirm_l.setText(" Confirm");
-        getContentPane().add(confirm_l, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 430, 80, -1));
-
-        confirm_bt.setIcon(new javax.swing.ImageIcon("D:\\NetBeans Projects\\PasswordGUI\\src\\img\\confirmButtonArm.png")); // NOI18N
+        confirm_bt.setIcon(new javax.swing.ImageIcon("OOP/src/Image/confirmButtonFont.png")); // NOI18N
         confirm_bt.setBorderPainted(false);
         confirm_bt.setContentAreaFilled(false);
+        confirm_bt.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                confirm_btMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                confirm_btMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                confirm_btMouseExited(evt);
+            }
+        });
         confirm_bt.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 confirm_btActionPerformed(evt);
             }
         });
-        getContentPane().add(confirm_bt, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 420, -1, -1));
+        getContentPane().add(confirm_bt, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 410, -1, -1));
 
-        bg.setIcon(new javax.swing.ImageIcon("D:\\NetBeans Projects\\PasswordGUI\\src\\img\\bgArm.png")); // NOI18N
+        bg.setBackground(new java.awt.Color(84, 59, 45));
+        bg.setIcon(new javax.swing.ImageIcon("OOP/src/Image/bgArm.png")); // NOI18N
+        bg.setOpaque(true);
         getContentPane().add(bg, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 30, -1, -1));
 
         pack();
@@ -136,11 +160,23 @@ public class Password extends javax.swing.JFrame {
 
     private void password_pwfFocusLost(java.awt.event.FocusEvent evt) {
 
-        if(password_pwf.getText().isEmpty()){
+        if(password_pwf.getText().equals("")){
             password_pwf.setText("Input Password");
         }
         //[153,153,153]
         password_pwf.setForeground(new Color(153,153,153));
+    }
+
+    private void confirm_btMouseExited(java.awt.event.MouseEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void confirm_btMouseEntered(java.awt.event.MouseEvent evt) {
+        // TODO add your handling code here:
+    }
+
+    private void confirm_btMouseClicked(java.awt.event.MouseEvent evt) {
+        // TODO add your handling code here:
     }
 
     /**
@@ -181,7 +217,6 @@ public class Password extends javax.swing.JFrame {
     // Variables declaration - do not modify
     private javax.swing.JLabel bg;
     private javax.swing.JButton confirm_bt;
-    private javax.swing.JLabel confirm_l;
     private javax.swing.JLabel enter_l;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel nqueue_l;
