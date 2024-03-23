69827de7988533aa9c0619af3fe061f787eeda8c
NowEXs <66070285@kmitl.ac.th>
2024-03-23 16:49:13 +0700

    Revert "Merge branch 'Db_system'"
    This reverts commit 2bdbd24320f4cc8bf2cd5b380c4dcc453182d961, reversing
    changes made to f2895e6830a054aea7e222ffb96c99160101804b.

--- OOP/src/CheckQueueMDI.java
+++ OOP/src/CheckQueueMDI.java
@@ -76,6 +76,7 @@ public class CheckQueueMDI extends javax.swing.JFrame {
 
         setLocationRelativeTo(null);
 
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
